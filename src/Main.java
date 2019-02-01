0d3d28a896b0271bda93faf93c7aa130104a934f
Matt Culin <culinm@gmail.com>
2019-02-01 09:43:54 -0500

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,34 @@
+import java.util.Scanner;
+
 public class Main
 {
 
     public static void main(String[] args)
     {
-        String cwd = System.getProperty("user.dir");
-        System.out.println("CWD: " + cwd);
+        Scanner scan = new Scanner(System.in);
+
+        int userInput;
+
+        System.out.print("Enter a value for n: ");
+        userInput = scan.nextInt();
+
+        fiboSeriesRec(userInput);
+        fiboSeriesIte(userInput);
+    }
+
+    public static int fiboSeriesRec(int n)
+    {
+        if (n<2)
+            return n;
+
+        return (n-1)+(n-2);
+    }
+
+    public static int fiboSeriesIte(int n)
+    {
+        for(int i = n; i > 1; i--)
+            return (n-1)+(n-2);
+
+        return n;
     }
 }
