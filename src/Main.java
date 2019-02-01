64deedcaec8591a5b2c5e1681b4f91102659e089
Matt Culin <culinm@gmail.com>
2019-02-01 10:32:04 -0500

    Third Commit

--- src/Main.java
+++ src/Main.java
@@ -12,11 +12,20 @@ public class Main
         System.out.print("Enter a value for n: ");
         userInput = scan.nextInt();
 
+        long startTimeRec = System.nanoTime();
         fiboSeriesRec(userInput);
+        long endTimeRec = System.nanoTime();
+        long timeElapsedRec = endTimeRec - startTimeRec;
+        System.out.println("Elapsed time in nanoseconds is: " + timeElapsedRec);
+
+        long startTimeIte = System.nanoTime();
         fiboSeriesIte(userInput);
+        long endTimeIte = System.nanoTime();
+        long timeElapsedIte = endTimeIte - startTimeIte;
+        System.out.println("Elapsed time in nanoseconds is: " + timeElapsedIte);
     }
 
-    public static int fiboSeriesRec(int n)
+    public static long fiboSeriesRec(int n)
     {
         if (n<2)
             return n;
@@ -24,7 +33,7 @@ public class Main
         return (n-1)+(n-2);
     }
 
-    public static int fiboSeriesIte(int n)
+    public static long fiboSeriesIte(int n)
     {
         for(int i = n; i > 1; i--)
             return (n-1)+(n-2);
